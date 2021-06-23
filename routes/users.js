c14e65912e45415b67f27d3a62fd1bbd074dff78
joyadauche <joy.adaeze.uchenna@gmail.com>
2021-06-23 15:29:09 +0100

    Update validation message

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { body, validationResult } = require('express-validator');
+const { validationResult, check } = require('express-validator');
 const randomstring = require("randomstring");
 const data = require('../data/users.json');
 const { validate, toJSON } = require('../models/user');
@@ -11,6 +11,7 @@ router.get('/', (req, res) => {
     res.send(data);
 });
 
+// simulating auth middleware assuming a request comes in to return a universally unique identifier for a user
 // router.get('/id', auth, (req, res) => {
 //     const user = data.find(user => user.email === req.user);
 
@@ -18,13 +19,17 @@ router.get('/', (req, res) => {
     
 //     res.send(user);
 // });
+
 router.get('/:id', (req, res) => {
     const user = data.find(user => user.id === req.params.id);
     if(!user) return res.status(404).send('User does not exist.'); 
     res.send(toJSON(user));
 })
 
-router.post('/', body('name').isLength({ min: 3 }).escape(), body('email').isEmail().escape(), (req, res) => {
+router.post('/', [
+    check('name').isLength({ min: 3 }).withMessage('Enter Name property that is minimum of 3 letters').escape(),
+    check('email').isEmail().withMessage('Email is Invalid').normalizeEmail()
+    ], (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
 
