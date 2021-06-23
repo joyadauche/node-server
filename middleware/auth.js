1c20b33bef556c5f7b14ed50f67ecda1e8a4b13f
joyadauche <joy.adaeze.uchenna@gmail.com>
2021-06-23 13:00:12 +0100

    Update spacing in auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,6 +4,7 @@ module.exports = function (req, res, next) {
     const token = req.header('x-auth-token');
     if (!token) return res.status(401).send('Access denied. No token provided.');
 
-      req.user = token; 
-      next();
+    req.user = token; 
+    
+    next();
 }
